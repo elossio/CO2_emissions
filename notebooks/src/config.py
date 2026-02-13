dff201692ebefba65b0fc428521c2645ea8c6ff7
Eduardo Lossio <evlossio@gmail.com>
2026-02-13 14:12:13 -0300

    "primeiro commit"

--- notebooks/src/config.py
+++ notebooks/src/config.py
@@ -6,8 +6,8 @@ PASTA_PROJETO = Path(__file__).resolve().parents[2]
 PASTA_DADOS = PASTA_PROJETO / "dados"
 
 # coloque abaixo o caminho para os arquivos de dados de seu projeto
-DADOS_ORIGINAIS = PASTA_DADOS / "housing.csv.zip"
-DADOS_LIMPOS = PASTA_DADOS / "housing_clean.parquet"
+DADOS_ORIGINAIS = PASTA_DADOS / "NTZ_02_CTE_LIQUIDO.csv"
+DADOS_LIMPOS = PASTA_DADOS / "NTZ_02_CTE_LIQUIDO_clean.parquet"
 DADOS_GEO_ORIGINAIS = PASTA_DADOS / "california_counties.geojson"
 DADOS_GEO_MEDIAN = PASTA_DADOS / "gdf_counties.parquet"
 
